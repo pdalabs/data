37b11d71bb81becce6f8f45dedb7e6aa0deb5112
pdalabs <pdaunofficial@gmail.com>
2025-03-27 12:42:02 +0530

    Add 11-12-27-03-2025_-_-ALL-folder_-_-u2.c

---
+++ files/11-12-27-03-2025_-_-ALL-folder_-_-u2.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<time.h>
+void selectionSort(int arr[],int n)
+{
+int i,j,minindex,temp;
+for(i=0;i<n-1;i++)
+{
+minindex=i;
+for(j=i+1;j<n;j++)
+{
+if(arr[j]<arr[minindex])
+{
+minindex=j;
+}
+}
+temp=arr[minindex];
+arr[minindex]=arr[i];
+arr[i]=temp;
+}
+}
+int main()
+{
+int n,i;
+clock_t start,end;
+double cpu_time_used;
+printf("Enter the number of elements:");
+scanf("%d",&n);
+int arr[n];
+printf("Enter %d elements :\n",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+start=clock();
+selectionSort(arr,n);
+end=clock();
+cpu_time_used = ((double)(end-start))/CLOCKS_PER_SEC;
+printf("Sorted elements:");
+for(i=0;i<n;i++)
+{
+printf("%d",arr[i]);
+}
+printf("\n");
+printf("Time taken to sort : %f seconds\n",cpu_time_used);
+
+
+return 0;
+}
