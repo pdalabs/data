7bf2f27df20f4b20ef44cf16bc39d728921464fd
pdalabs <pdaunofficial@gmail.com>
2025-04-03 12:23:59 +0530

    Add 23-12-03-04-2025_-_-ALL-folder_-_-lab4.c

---
+++ files/23-12-03-04-2025_-_-ALL-folder_-_-lab4.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int i,j,n,r=-1,f,q[20],visited[20];
+int a[20][20];
+int bfs(int v);
+void main()
+{
+int v;
+printf("Enter the number of vertices....\n");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+q[i]=0;
+visited[i]=0;
+}
+printf("Enter the adjacency matrix...\n");
+for(i=1;i<=n;i++)
+for(j=1;j<=n;j++)
+scanf("%d",& a[i][j]);
+printf("Enter the starting vertex...\n");
+scanf("%d",&v);
+bfs(v);
+printf("The nodes that are reachable from given node %d are..\n",v);
+for(i=1;i<=n;i++)
+if(visited[i])
+printf("\t%d",i);
+printf("\n");
+}
+int bfs(int v)
+{
+for(i=1;i<=n;i++)
+if(a[v][i]&& !visited[i])
+q[++r]=i;
+visited[i]=1;
+if(f<=r)
+{
+visited[q[f]]=1;
+bfs(q[f++]);
+}
+}
