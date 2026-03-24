eb3416e8c939ce473ed6c8921c1c5ffbc00307d3
pdalabs <pdaunofficial@gmail.com>
2026-03-24 10:21:44 +0530

    Add 21-10-24-03-2026_-_-ALL-folder_-_-lab8.py

---
+++ files/21-10-24-03-2026_-_-ALL-folder_-_-lab8.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+image=cv2.imread('ll.jpg')
+cv2.imshow('title',image)
+image_gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+kernel=np.ones((3,3),np.uint8)
+dilated=cv2.dilate(image_gray,kernel,iterations=2)
+eroded=cv2.erode(image_gray,kernel,iterations=2)
+opening=cv2.morphologyEx(image_gray,cv2.MORPH_OPEN,kernel)
+closing=cv2.morphologyEx(image_gray,cv2.MORPH_CLOSE,kernel)
+fig,axs=plt.subplots(2,2,figsize=(7,7))
+axs[0,0].imshow(dilated,cmap='Greys')
+axs[0,0].set_title('Dilated image')
+axs[0,0].set_xticks([])
+axs[0,0].set_yticks([])
+
+
+axs[0,1].imshow(dilated,cmap='Greys')
+axs[0,1].set_title('Eroded image')
+axs[0,1].set_xticks([])
+axs[0,1].set_yticks([])
+
+axs[1,0].imshow(opening,cmap='Greys')
+axs[1,0].set_title('Opening')
+axs[1,0].set_xticks([])
+axs[1,0].set_yticks([])
+
+axs[1,1].imshow(closing,cmap='Greys')
+axs[1,1].set_title('Closing')
+axs[1,1].set_xticks([])
+axs[1,1].set_yticks([])
+
+plt.tight_layout()
+plt.show()
