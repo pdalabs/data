db42be5ee0447571ef84f193048138043e31cc2c
pdalabs <pdaunofficial@gmail.com>
2026-08-28 12:00:56 +0530

    Add 00-12-28-08-2026_-_-ALL-folder_-_-prg1.py

---
+++ files/00-12-28-08-2026_-_-ALL-folder_-_-prg1.py
@@ -0,0 +1,27 @@
+import hashlib
+
+def get_digests(data: str):
+    byte_data = data.encode('utf-8')
+
+    sha1 = hashlib.sha1(byte_data).hexdigest()
+    sha224 = hashlib.sha224(byte_data).hexdigest()
+    sha256 = hashlib.sha256(byte_data).hexdigest()
+    sha384 = hashlib.sha384(byte_data).hexdigest()
+    sha512 = hashlib.sha512(byte_data).hexdigest()
+
+    return {
+        'SHA-1': sha1,
+        'SHA-224': sha224,
+        'SHA-256': sha256,
+        'SHA-384': sha384,
+        'SHA-512': sha512
+    }
+
+
+if __name__ == "__main__":
+    input_data = input("Enter the string to hash: ")
+
+    digests = get_digests(input_data)
+
+    for algo, digest in digests.items():
+        print(f"{algo} digest: {digest}")
