f0dc2a2d5aa8dfecfc67a9f010c08153631ae1fc
pdalabs <pdaunofficial@gmail.com>
2026-08-28 12:02:40 +0530

    Add 02-12-28-08-2026_-_-eh lab-folder_-_-eh prg2b.py

---
+++ files/02-12-28-08-2026_-_-eh lab-folder_-_-eh prg2b.py
@@ -0,0 +1,21 @@
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric  import padding
+from cryptography.hazmat.primitives import serialization
+with open("public_key.pem","rb")as f:
+     public_key=serialization.load_pem_public_key(f.read())
+with open("Confidential.txt","rb")as file:
+    file_data=file.read()
+with open("confidential.signature","rb")as sig_file:
+          signature=sig_file.read()
+try:
+    public_key.verify(
+        signature,
+        file_data,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH,),
+        hashes.SHA256()
+        )
+    print("signature is valid")
+except Exception as e:
+    print("signature is not valid:",str(e))
